7514d55242ade9901f7f18dd81ddb5ff28da69e1
Coach Clint <clint.l.stevens@gmail.com>
2026-09-04 15:54:43 -0600

    Wrap Superuser preview in shared Owner Operator workspace shell

--- app/preview/superuser/page.tsx
+++ app/preview/superuser/page.tsx
@@ -1,3 +1,6 @@
 import SuperuserHome from '../../app/superuser/page';
+import PreviewShell from '../PreviewShell';
 export const dynamic = 'force-dynamic';
-export default SuperuserHome;
+export default async function SuperuserPreviewPage() {
+  return <PreviewShell role="superuser"><SuperuserHome /></PreviewShell>;
+}
